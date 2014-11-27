382d7cac63c7589805b6b619d3b4fc293ca4a265
David Christian <david.w.christian@gmail.com>
2014-11-27 01:37:43 -0800

    changed aws congfig 2

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,3 @@
-AWS.config( access_key_id: '##', 
-            secret_access_key: '##', 
-            region: '##')
+AWS.config( access_key_id: 'AKIAIF3ROBP4XD3EUVIQ', 
+            secret_access_key: '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', 
+            region: 'us-west-1')
