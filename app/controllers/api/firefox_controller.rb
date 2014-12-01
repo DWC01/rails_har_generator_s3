a480062f1c8166237b5ef38124c5dbbecaeae62e
David Christian <david.w.christian@gmail.com>
2014-12-01 13:33:43 -0800

    added abiltiy to send all params as json

--- app/controllers/api/firefox_controller.rb
+++ app/controllers/api/firefox_controller.rb
@@ -1,10 +1,13 @@
+# TEST URL
+# curl -d '{"url":"http://www.google.com", "user_id": "2", "model_name":"ad_tag_validation", "model_id":"01", "id":"01"}' http://localhost:3000/api/test_url/ --header "Accept: application/json" --header "Content-Type: application/json"
 module Api
   class FirefoxController < ApplicationController
     protect_from_forgery with: :null_session
     respond_to :json
 
     def test_url
-      url = "http://displayadtech.com"
+      url = params[:url]
+      puts export_params
       har = HarFile.new(export_params)
       Firefox.export_har(url, har.export_path)
       save_to_s3(har.export_path, har.unique_path, har.file_name)
