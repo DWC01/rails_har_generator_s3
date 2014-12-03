1e9da1c595b74a3b18008c23ead3d3c56ed13433
David Christian <david.w.christian@gmail.com>
2014-12-03 15:12:38 -0800

    added ENV vars for AWS initialize

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,3 +1,3 @@
-AWS.config( access_key_id: '##', 
-            secret_access_key: '##', 
-            region: '##')
+AWS.config( access_key_id: ENV['AWS_ACCESS_KEY'], 
+            secret_access_key: ENV['AWS_SECRET_KEY'],
+            region: 'us-west-1')
